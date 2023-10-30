9330835281ba1577a2e1692e88dfb9b54c1a8bd0
suzuuuuu09 <bsamu5788@gmail.com>
2023-10-30 22:14:45 +0900

    Update

--- Typing_GUI.py
+++ Typing_GUI.py
@@ -8,7 +8,7 @@ window = sg.Window("hogehoge", layout, size=(1920, 1080))
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "OK":
-        f = open("target1900.txt", "r")
+        f = open("word.txt", "r")
         l = f.readlines()
         s = random.sample(1, 20)
         window["-TEXT-"].update(s)
