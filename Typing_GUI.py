89956b5fce65338e82f32e130cdae488fdde110c
suzuuuuu09 <bsamu5788@gmail.com>
2023-10-31 10:30:15 +0900

    Update

--- Typing_GUI.py
+++ Typing_GUI.py
@@ -1,7 +1,12 @@
 import PySimpleGUI as sg
 import tkinter as tk
-import Typing_Game
+import random, keyboard, pygame, pyautogui
 
+#word.txtからランダム抽出
+lines = open('word.txt').read().splitlines()
+myline =random.choice(lines)
+
+#GUIのテーマ
 sg.theme('SystemDefault')
 
 canvas = sg.Canvas(size=(1280,720))
@@ -9,8 +14,8 @@ layout = [[canvas]]
 
 window = sg.Window('サンプル', layout, finalize=True)
 
-canvas.tk_canvas.create_text(640, 360, text="fuck", font=("HG丸ｺﾞｼｯｸM-PRO",46))
-
+canvas.tk_canvas.create_text(640, 360, text=myline, font=("HG丸ｺﾞｼｯｸM-PRO",46))
+keyboard.write('The quick brown fox jumps over the lazy dog.')
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
